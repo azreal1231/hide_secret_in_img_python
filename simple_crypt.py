4e71b7ec5ad333fcc945e69d699d9a0c16aa59f8
azreal <edward@slatelight.co.za>
2022-02-05 15:10:55 +0200

    ? removed unused files

--- simple_crypt.py
+++
@@ -1,9 +0,0 @@
-import cryptocode
-
-KEY = 'HALO is cool'
-text = 'master chief is DEAD!'
-
-str_encoded = cryptocode.encrypt(text, KEY)
-str_decoded = cryptocode.decrypt(str_encoded, KEY)
-print(str_encoded)
-print(str_decoded)
