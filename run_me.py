15ba5c6d15ea2413e07bda9103540f3825c992d0
azreal <edward@slatelight.co.za>
2022-02-05 15:14:06 +0200

    ? renamed run_me.py

--- run_me.py
+++ run_me.py
@@ -1,7 +1,13 @@
-from termcolor import colored
-from stegano import lsb
-import cryptocode
-import json
+try:
+    from termcolor import colored
+    from stegano import lsb
+    import cryptocode
+    import getpass
+    import json
+except Exception as e:
+    print('Please install All required PIP Modules!')
+    print(str(e))
+    exit()
 
 
 def check_if_file_exists(_file_name):
@@ -20,19 +26,23 @@ def check_if_file_exists(_file_name):
     return file
 
 
+def get_encrypt_key():
+    return getpass.getpass('Enter Encryption Key... ')
+
+
 def start():
     print(colored('Enter The One Following Options Continue', 'cyan'))
     print(f'Option {colored("1", "magenta")} hides a secret in an image.')
     print(f'Option {colored("2", "magenta")} gets a secret from and image.')
 
-    option = int(input('... '))
-    if option not in [1, 2]:
+    option = input('... ')
+    if option not in ['1', '2']:
         print(f' {colored(str(option), "red")} is not a valid option!')
         exit()
 
-    if option == 1:
+    if option == '1':
         hide()
-    elif option == 2:
+    elif option == '2':
         show()
     else:
         raise Exception('Something Went Wrong...')
@@ -43,20 +53,17 @@ def hide():
 
     def ask_for_encryption():
         awns = ['yes', 'no']
-        awn = input(f'Would You Like To Encrypt Your Secret Text {colored("YES", "green")}/{colored("NO", "red")}... ')
+        awn = input(f'Would You Like To Encrypt Your Secret Text {colored("YES", "green")}/{colored("NO", "red")} ')
         awn = awn.lower()
         valid_awn = True if awn in awns else False
         while not valid_awn:
             print(colored(f'{awn} is not a Valid Answer!\n', 'red'))
             awn = input(f'Would You Like To Encrypt Your Secret Text '
-                        f'{colored("YES", "green")}/{colored("NO", "red")}... ')
+                        f'{colored("YES", "green")}/{colored("NO", "red")} ')
             awn = awn.lower()
             valid_awn = True if awn in awns else False
 
         return awn
-
-    def get_encrypt_key():
-        return input('Enter Encryption Key... ')
 
     def encrypt_text(_key, _text):
         str_encoded = cryptocode.encrypt(_text, _key)
@@ -71,14 +78,15 @@ def hide():
         try:
             secret = lsb.hide(_file, json_data)
             new_file = f"encrypted.png"
-            print('Encrypted File Name:  ' + new_file + '\n')
+            print('Encrypted File Name:  ' + colored(new_file, 'cyan') + '\n')
             secret.save(new_file)
-            return colored(f'Successfully Encrypted Img With Secret Message.\n', 'green')
+            print(f"{colored(f'Success', 'green')} Encrypted Img With Secret Message.\n")
         except Exception as e:
-            print(e)
-            return colored(f'Failed To Encrypt Img With Secret Message!.\n', 'red')
+            print(f"{colored('Warning: ', 'yellow')} Failed To Encrypt Img With Secret Message!")
+            print(f"{colored('Error: ', 'red')} {str(e)}")
+            exit()
 
-    file_to_embed_data_into = input('Please Enter File Name: ')
+    file_to_embed_data_into = input('Please Enter Filename: ')
     file_to_embed_data_into = check_if_file_exists(file_to_embed_data_into)
 
     encrypt = ask_for_encryption()
@@ -90,8 +98,7 @@ def hide():
     if encrypt == 'yes':
         secret_text = encrypt_text(key, secret_text)
 
-    result = encrypt_img(file_to_embed_data_into, encrypt, secret_text)
-    print(result)
+    encrypt_img(file_to_embed_data_into, encrypt, secret_text)
 
 
 def show():
@@ -110,14 +117,14 @@ def show():
     data = json.loads(raw_data)
     if data['encrypted']:
         print('Data Has Been Encrypted!')
-        key = input('Please Enter Decryption Key... ')
+        key = get_encrypt_key()
         str_decoded = cryptocode.decrypt(data['text'], key)
         if not str_decoded:
-            print(colored('Incorrect Decryption KEY!', 'red'))
+            print(f"{colored('Warning', 'yellow')} Incorrect Decryption KEY!")
         else:
-            print(f"\nSecret Message: {str_decoded}")
+            print(f"\n{colored('Secret Message: ', 'cyan')} {str_decoded}")
     else:
-        print(f"\nSecret Message: {data['text']}")
+        print(f"\n{colored('Secret Message: ', 'cyan')} {data['text']}")
 
 
 if __name__ == '__main__':
